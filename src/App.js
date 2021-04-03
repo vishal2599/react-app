32f6cd176976f5a5963e1985cb5ab5c8409f51d6
vishal2599 <vshl2599@gmail.com>
2021-04-03 17:35:48 +0530

    Using State in Function => sideNote: It is super confusing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonState] = useState({
     persons : [
       { role: 'Developer', age: 25 },
       { role: 'Q/A', age: 27 },
       { role: 'Designer', age: 29 }
-    ],
-    otherState: 'some other value'
-  }
+    ]
+  });
 
-  switchRoleHandler = () => {
+  const [otherState, setOtherState] = useState({otherState: 'some other value'});
+  console.log(personsState, otherState);
+
+
+  const switchRoleHandler = () => {
     // NEVER DO THIS:  this.state.persons[0].role = 'Project Manager';
-    this.setState({persons:[
+    setPersonState({persons:[
         { role: 'Project Manager', age: 25 },
         { role: 'Q/A', age: 27 },
         { role: 'Designer', age: 35 }
       ]
     });
-  }
+  };
 
-  render() {
     return (
       <div className="App">
         <h1>Hi, I am a React Developer.</h1>
-        <button onClick={this.switchRoleHandler}>Switch Role</button>
-        <Person role={this.state.persons[0].role} age={this.state.persons[0].age}/>
-        <Person role={this.state.persons[1].role} age={this.state.persons[1].age}> My hobby is to go on long bike rides. </Person>
-        <Person role={this.state.persons[2].role} age={this.state.persons[2].age} />
+        <button onClick={switchRoleHandler}>Switch Role</button>
+        <Person role={personsState.persons[0].role} age={personsState.persons[0].age}/>
+        <Person role={personsState.persons[1].role} age={personsState.persons[1].age}> My hobby is to go on long bike rides. </Person>
+        <Person role={personsState.persons[2].role} age={personsState.persons[2].age} />
       </div>
     );
     // return React.createElement('div',  {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
-}
+    };
 
-export default App;
+export default app;
+
+// state = {
+  
+// };
+

03289de14fd4efa4c95916db1c02a4a4b9fb782f
vishal2599 <vshl2599@gmail.com>
2021-04-03 20:24:21 +0530

    Understood the base features and Syntax

--- src/App.js
+++ src/App.js
@@ -1,46 +1,67 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonState] = useState({
+class App extends Component {
+  state = {
     persons : [
-      { role: 'Developer', age: 25 },
-      { role: 'Q/A', age: 27 },
-      { role: 'Designer', age: 29 }
-    ]
-  });
+      { name: 'Developer', age: 25 },
+      { name: 'DevOps', age: 27 },
+      { name: 'Designer', age: 29 }
+    ],
+    otherState: 'some other value'
+  }
 
-  const [otherState, setOtherState] = useState({otherState: 'some other value'});
-  console.log(personsState, otherState);
-
-
-  const switchRoleHandler = () => {
-    // NEVER DO THIS:  this.state.persons[0].role = 'Project Manager';
-    setPersonState({persons:[
-        { role: 'Project Manager', age: 25 },
-        { role: 'Q/A', age: 27 },
-        { role: 'Designer', age: 35 }
+  switchNameHandler = (newName) => {
+    // NEVER DO THIS:  this.state.persons[0].name = 'Project Manager';
+    this.setState({persons:[
+        { name: newName, age: 25 },
+        { name: 'Sumit', age: 27 },
+        { name: 'Rattan', age: 35 }
       ]
     });
+  }
+
+  nameChangedHandler = event =>{
+    this.setState({persons:[
+      { name: 'Vish!', age: 25 },
+      { name: event.target.value, age: 27 },
+      { name: 'Rattan', age: 35 }
+    ]
+  });
   };
 
+  render() {
+    const style = {
+      backgroundColor: '#fff',
+      font: 'inherit',
+      border: '2px solid dodgerblue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I am a React Developer.</h1>
-        <button onClick={switchRoleHandler}>Switch Role</button>
-        <Person role={personsState.persons[0].role} age={personsState.persons[0].age}/>
-        <Person role={personsState.persons[1].role} age={personsState.persons[1].age}> My hobby is to go on long bike rides. </Person>
-        <Person role={personsState.persons[2].role} age={personsState.persons[2].age} />
+        <button 
+        onClick={() => this.switchNameHandler('Vishu')}
+        style={style}>Switch name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Vishal Singh')}
+        changed={this.nameChangedHandler}> My hobby is to go on long bike rides. </Person>
+ 
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div',  {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-    };
+  }
+}
 
-export default app;
-
-// state = {
-  
-// };
-
+export default App;
